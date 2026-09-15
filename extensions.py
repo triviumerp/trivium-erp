20d343c7d4512069a668b12a52a4d5855b3477d4
sergiodevcavalcante <sergio.cavalcante666@gmail.com>
2026-09-15 20:56:56 -0300

    Fix: correcao de templates (cadastro/detalhe_cliente), models e extensions

--- extensions.py
+++ extensions.py
@@ -6,6 +6,3 @@ from flask_limiter.util import get_remote_address
 db = SQLAlchemy()
 login_manager = LoginManager()
 limiter = Limiter(key_func=get_remote_address, default_limits=["200 per day", "50 per hour"])
-
-db = SQLAlchemy()
-login_manager = LoginManager()
